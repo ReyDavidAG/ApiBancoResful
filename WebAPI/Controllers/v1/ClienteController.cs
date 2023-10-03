0363c3bad4bf2438b2685f6b87ab50e091772810
ReyDavidAG <david.aguila@proan.com>
2023-10-03 16:48:52 -0600

    Add - ClienteController

---
+++ WebAPI/Controllers/v1/ClienteController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Clientes.Commands.CreateClienteCommand;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ClienteController : BaseAPIController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateClienteCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
