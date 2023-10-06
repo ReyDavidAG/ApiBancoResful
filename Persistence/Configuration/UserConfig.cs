ea6ba1135382d73086659d507d896402b43195a6
ReyDavidAG <david.aguila@proan.com>
2023-10-06 15:35:16 -0600

    Add Configuration Model - UserConfig

---
+++ Persistence/Configuration/UserConfig.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configuration
+{
+    public class UserConfig : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.ToTable("Usersss");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nombre)
+                .HasMaxLength(80)
+                .IsRequired();
+
+            builder.Property(p => p.Apellido)
+                .HasMaxLength(80)
+                .IsRequired();
+
+            builder.Property(p => p.UserName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.Email)
+                .HasMaxLength(100)
+                .IsRequired();
+
+        }
+    }
+}
