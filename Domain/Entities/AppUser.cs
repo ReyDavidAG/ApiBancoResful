32c73aeab3d835a0818d4756217969d80c4352b4
ReyDavidAG <david.aguila@proan.com>
2023-10-06 15:34:48 -0600

    Add Model - AppUser : IdentityUser

---
+++ Domain/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class AppUser : IdentityUser 
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+    }
+}
