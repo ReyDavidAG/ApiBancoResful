b4613adae7d73d9f5623018cbee80c31757729b4
ReyDavidAG <david.aguila@proan.com>
2023-10-09 16:51:22 -0600

    Add Query - GetAllUsers

---
+++ Application/Features/User/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Dtos;
+using MediatR;
+
+namespace Application.Features.Users.Queries
+{
+    public class GetAllUsersQuery : IRequest<List<UserDatosDto>>
+    {
+        public string? Nombre { get; set; }
+    }
+}
