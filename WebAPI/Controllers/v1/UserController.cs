bd27b33cc8f26cf11790ba29bb9336ad0ca660ed
ReyDavidAG <david.aguila@proan.com>
2023-10-09 16:47:01 -0600

    Add Controller - User Controller

---
+++ WebAPI/Controllers/v1/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.User.Commands;
+using Application.Features.Users.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class UsersController : BaseAPIController
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetAllUsersQuery filter)
+        {
+            return Ok(await Mediator.Send(new GetAllUsersQuery { Nombre = filter.Nombre }));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            return Ok(await Mediator.Send(new GetUserByIdQuery { Id = id }));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateUserCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
