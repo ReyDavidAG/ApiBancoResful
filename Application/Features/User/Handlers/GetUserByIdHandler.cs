975615a9718c9c1027999e7f73fc3acfdba6a1e8
ReyDavidAG <david.aguila@proan.com>
2023-10-09 16:50:46 -0600

    Add Handler - GetUserById

---
+++ Application/Features/User/Handlers/GetUserByIdHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Users.Queries;
+using Application.Interfaces;
+using Application.Wrappers;
+using AutoMapper;
+using Domain.Dtos;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Users.Handlers
+{
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, Response<UserDatosDto>>
+    {
+        private readonly IRepositoryAsync<AppUser> _repositoryAsync;
+        private readonly IMapper _mapper;
+        public GetUserByIdHandler(IRepositoryAsync<AppUser> repositoryAsync, IMapper mapper)
+        {
+            _repositoryAsync = repositoryAsync;
+            _mapper = mapper;
+        }
+        public async Task<Response<UserDatosDto>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _repositoryAsync.GetByIdAsync(request.Id);
+
+            if (user == null)
+                throw new KeyNotFoundException($"Regustro no encontrado con el id {request.Id}");
+
+            var result = _mapper.Map<UserDatosDto>(user);
+
+            return new Response<UserDatosDto>(result);
+        }
+    }
+}
