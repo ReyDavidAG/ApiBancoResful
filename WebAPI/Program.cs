8214ae5c387e19815514479c5d92395f605f6714
ReyDavidAG <david.aguila@proan.com>
2023-10-09 17:34:03 -0600

    Add Configuration - Swagger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,15 @@
 using Application;
 using Domain.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Persistence;
 using Persistence.Context;
 using Shared;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
 using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +19,60 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secreta");
+
+#region Authentication
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+#endregion
+
+#region Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description =
+        "Autentication JWT usando el esquea Bearer. \r\n\r\n " +
+        "Ingresa tu token \r\n\r\n" +
+        "Ejemplo: \" token\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme="oauth2",
+                Name ="Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+#endregion
 
 //Agregar servicios Application
 #region Application
