b768944cbf0a819245e86f51142bdd58f66c86ec
ReyDavidAG <david.aguila@proan.com>
2023-10-09 16:45:10 -0600

    Add Command - CreateUser

---
+++ Application/Features/User/Commands/CreateUserCommand.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Application.Wrappers;
+using Domain.Dtos;
+using MediatR;
+
+namespace Application.Features.User.Commands
+{
+    public class CreateUserCommand : IRequest<Response<UserDatosDto>>
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email{ get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
