c332f53cf4be5058e11ac805daf11d0b8a022383
ReyDavidAG <david.aguila@proan.com>
2023-10-09 16:50:14 -0600

    Add Query - GetUserById

---
+++ Application/Features/User/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Wrappers;
+using Domain.Dtos;
+using MediatR;
+
+namespace Application.Features.Users.Queries
+{
+    public class GetUserByIdQuery : IRequest<Response<UserDatosDto>>
+    {
+        public string Id { get; set; }
+    }
+}
