ab074e57ebe777ffc5378827eeff0729b1e419ab
ReyDavidAG <david.aguila@proan.com>
2023-10-10 15:23:31 -0600

    Add Controller - AccountController

---
+++ WebAPI/Controllers/v1/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Acounts.Commands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class AccountController : BaseAPIController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] SignInCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
