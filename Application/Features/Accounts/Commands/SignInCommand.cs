7c87aca0a0cffde0158d15e02128c5ec97c62705
ReyDavidAG <david.aguila@proan.com>
2023-10-10 15:25:13 -0600

    Add Command - SignInCommand

---
+++ Application/Features/Accounts/Commands/SignInCommand.cs
@@ -0,0 +1,17 @@
+﻿using Application.Wrappers;
+using Domain.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Acounts.Commands
+{
+    public class SignInCommand : IRequest<Response<UserLoginResponseDto>>
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
