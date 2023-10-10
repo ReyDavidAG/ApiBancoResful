b4502318d6bd6f819ef9c5faad22dc325317a11a
ReyDavidAG <david.aguila@proan.com>
2023-10-10 15:26:34 -0600

    Add Validator - SignInCommandValidator

---
+++ Application/Features/Accounts/Validators/SignInCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Acounts.Commands;
+using Application.Interfaces;
+using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Features.Acounts.Validators
+{
+    public class SignInCommandValidator : AbstractValidator<SignInCommand>
+    {
+        public SignInCommandValidator(IRepositoryAsync<AppUser> repositoryAsync)
+        {
+            RuleFor(u => u.UserName)
+                .NotEmpty().WithMessage("{PropertyName} es requerido.");
+
+            RuleFor(u => u.Password)
+                .NotEmpty().WithMessage("{PropertyName} es requerido.");
+        }
+    }
+}
