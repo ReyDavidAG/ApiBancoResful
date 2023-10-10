a0e347bf8a6b9a4c5f734ec73f5e439033417cfd
ReyDavidAG <david.aguila@proan.com>
2023-10-10 15:20:12 -0600

    Fix Controller - UserController

--- WebAPI/Controllers/v1/UserController.cs
+++ WebAPI/Controllers/v1/UserController.cs
@@ -1,23 +1,31 @@
 ﻿using Application.Features.User.Commands;
 using Application.Features.Users.Queries;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers.v1
 {
     [ApiVersion("1.0")]
+    [Authorize(Roles = "admin")]
     public class UsersController : BaseAPIController
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Get([FromQuery] GetAllUsersQuery filter)
         {
             return Ok(await Mediator.Send(new GetAllUsersQuery { Nombre = filter.Nombre }));
         }
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetById(string id)
         {
             return Ok(await Mediator.Send(new GetUserByIdQuery { Id = id }));
         }
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Post(CreateUserCommand command)
         {
             return Ok(await Mediator.Send(command));
