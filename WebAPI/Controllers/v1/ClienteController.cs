1ce6e5e80e991a12a35378d369c202fbbe2b7e53
ReyDavidAG <david.aguila@proan.com>
2023-10-10 15:20:41 -0600

    Fix Controller - ClienteController

--- WebAPI/Controllers/v1/ClienteController.cs
+++ WebAPI/Controllers/v1/ClienteController.cs
@@ -2,28 +2,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Application.Features.Clientes.Commands;
 using Application.Features.Clientes.Queries;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebAPI.Controllers.v1
 {
     [ApiVersion("1.0")]
+    [Authorize(Roles = "basic")]
     public class ClienteController : BaseAPIController
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Get([FromQuery] GetAllClientesParameters filter)
         {
             return Ok(await Mediator.Send(new GetAllClientesQuery { PageNumber = filter.PageNumber, PageSize = filter.PageSize, Nombre = filter.Nombre, Apellido = filter.Apellido }));
         }
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Get(int id)
         {
             return Ok(await Mediator.Send(new GetClienteByIdQuery { Id = id }));
         }
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Post(CreateClienteCommand command)
         {
             return Ok(await Mediator.Send(command));
         }
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Put(int id, UpdateClienteCommand command)
         {
             if (id != command.Id)
@@ -32,6 +42,8 @@ namespace WebAPI.Controllers.v1
             return Ok(await Mediator.Send(command));
         }
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Delete(int id)
         {
             return Ok(await Mediator.Send(new DeleteClienteCommand { Id = id }));
