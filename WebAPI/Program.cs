948cf8f3580efb6218e2054659f84446dc03462e
ReyDavidAG <david.aguila@proan.com>
2023-10-10 15:19:43 -0600

    Update Program - Configuration Authentication

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,25 +2,31 @@ using Application;
 using Domain.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Persistence.Context;
 using Shared;
-using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text;
 using WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-var key = builder.Configuration.GetValue<string>("ApiSettings:Secreta");
+var key = builder.Configuration["ApiSettings:Secreta"];
+//Agregar servicios Application
+#region Application
+builder.Services.AddApplication();
+#endregion
+#region Infraestructure
+builder.Services.AddPersistenceInfraestructure(builder.Configuration);
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+#endregion
+#region SharedInfraestructure
+builder.Services.AddSharedInfraestructure(builder.Configuration);
+#endregion
+#region Version
+builder.Services.AddApiVersioningExtension();
+#endregion
 
 #region Authentication
 builder.Services.AddAuthentication(x =>
@@ -38,9 +44,22 @@ builder.Services.AddAuthentication(x =>
         ValidateIssuer = false,
         ValidateAudience = false
     };
+    x.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = context =>
+        {
+            if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                context.Response.Headers.Add("token-Expired", "true");
+
+            return Task.CompletedTask;
+        }
+    };
 });
 #endregion
-
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
 #region Swagger
 builder.Services.AddSwaggerGen(options =>
 {
@@ -48,8 +67,8 @@ builder.Services.AddSwaggerGen(options =>
     {
         Description =
         "Autentication JWT usando el esquea Bearer. \r\n\r\n " +
-        "Ingresa tu token \r\n\r\n" +
-        "Ejemplo: \" token\"",
+        "Ingresa la palabra 'Bearer' seguida de un [espacio] y despues su token en el campo de abajo \r\n\r\n" +
+        "Ejemplo: \"Bearer tkskysykkys\"",
         Name = "Authorization",
         In = ParameterLocation.Header,
         Scheme = "Bearer"
@@ -73,21 +92,10 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 #endregion
-
-//Agregar servicios Application
-#region Application
-builder.Services.AddApplication();
-#endregion
-#region Infraestructure
-builder.Services.AddPersistenceInfraestructure(builder.Configuration);
-builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
-#endregion
-#region SharedInfraestructure
-builder.Services.AddSharedInfraestructure(builder.Configuration);
-#endregion
-#region Version
-builder.Services.AddApiVersioningExtension();
-#endregion
+builder.Services.AddCors(p => p.AddPolicy("PolicyCors", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -98,6 +106,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("PolicyCors");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
