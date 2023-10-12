8d5d31122088e47e9b70383e665c29258d8a85ac
ReyDavidAG <david.aguila@proan.com>
2023-10-12 16:11:02 -0600

    Add Validator - JwtRequestValidator

---
+++ Application/Features/Accounts/Validators/JwtRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Accounts.Commands;
+using Application.Interfaces;
+using FluentValidation;
+
+namespace Application.Features.Accounts.Validators;
+
+public class JwtRequestValidator : AbstractValidator<JWTRequest>
+{
+    public JwtRequestValidator()
+    {
+        RuleFor(j => j.RefreshToken)
+          .NotEmpty().WithMessage("{PropertyName} no puede estar vacio.");
+    }
+}
