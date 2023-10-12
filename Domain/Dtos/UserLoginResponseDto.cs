3775ece12f0ba6597281da9c20702bbe673218cb
ReyDavidAG <david.aguila@proan.com>
2023-10-12 16:07:00 -0600

    Fix Dto - UserLoginResponseDto

--- Domain/Dtos/UserLoginResponseDto.cs
+++ Domain/Dtos/UserLoginResponseDto.cs
@@ -2,8 +2,13 @@
 {
     public class UserLoginResponseDto
     {
+        public string Id { get; set; }
         public UserDatosDto User { get; set; }
         public string Token { get; set; }
         public DateTime? Expires { get; set; }
+        public int UserId { get; set; }
+        public List<string> Roles { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? refreshExpires { get; set;}
     }
 }
