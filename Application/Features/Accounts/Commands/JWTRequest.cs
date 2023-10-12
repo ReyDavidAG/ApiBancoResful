c18126c4f3ab0411a5e7e436bb4a9ecfcdb8134f
ReyDavidAG <david.aguila@proan.com>
2023-10-12 16:10:32 -0600

    Add Command - JwtRequest

---
+++ Application/Features/Accounts/Commands/JWTRequest.cs
@@ -0,0 +1,10 @@
+﻿using Application.Wrappers;
+using Domain.Dtos;
+using MediatR;
+
+namespace Application.Features.Accounts.Commands;
+
+public class JWTRequest : IRequest<Response<JwtResponseDto>>
+{
+    public string RefreshToken { get; set; }
+}
