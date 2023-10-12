c2cc4a51a4507fb6a62f496432ac0b3864769506
ReyDavidAG <david.aguila@proan.com>
2023-10-12 15:44:30 -0600

    Fix SignIn Command Dependencies

--- Application/Features/Accounts/Commands/SignInCommand.cs
+++ Application/Features/Accounts/Commands/SignInCommand.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Wrappers;
 using Domain.Dtos;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.Acounts.Commands
 {
