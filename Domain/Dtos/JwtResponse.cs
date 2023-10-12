ba9aa82089c1e87b1f0ca0c08a53c4621a734f92
ReyDavidAG <david.aguila@proan.com>
2023-10-12 16:03:37 -0600

    Add Dto - JwtResponse

---
+++ Domain/Dtos/JwtResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Domain.Dtos;
+
+public class JwtResponse
+{
+    public JwtSecurityToken Token { get; set; }
+    public DateTime Expires { get; set; }
+    public List<string> Roles { get; set; }
+}
