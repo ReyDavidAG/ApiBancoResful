62a1180c80391c21b0dc05412504c6dbfe2ff441
ReyDavidAG <david.aguila@proan.com>
2023-10-12 16:12:04 -0600

    Add Command - Post RefreshToken In Controller

--- WebAPI/Controllers/v1/AccountController.cs
+++ WebAPI/Controllers/v1/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Features.Acounts.Commands;
+﻿using Application.Features.Accounts.Commands;
+using Application.Features.Acounts.Commands;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,15 @@ namespace WebAPI.Controllers.v1
     [ApiVersion("1.0")]
     public class AccountController : BaseAPIController
     {
-        [HttpPost]
+        [HttpPost("Login")]
         public async Task<IActionResult> Post([FromBody] SignInCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+        [HttpPost("RefreshToken")]
+        public async Task<IActionResult> Refresh([FromBody] JWTRequest request)
+        {
+            return Ok(await Mediator.Send(request));
         }
     }
 }
