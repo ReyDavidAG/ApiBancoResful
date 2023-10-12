019fefee6e23474a3c4c312ede648224f3ea6de7
ReyDavidAG <david.aguila@proan.com>
2023-10-12 15:45:52 -0600

    Fix Handler - CreateUserHandler

--- Application/Features/User/Handlers/CreateUserHandler.cs
+++ Application/Features/User/Handlers/CreateUserHandler.cs
@@ -9,18 +9,22 @@ using Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Application.Features.User.Handlers
 {
     public class CreateUserHandler : IRequestHandler<CreateUserCommand, Response<UserDatosDto>>
     {
         private readonly IRepositoryAsync<AppUser> _repositoryAsync;
+        private readonly IRepositoryAsync<Domain.Entities.User> _repositoryAsync1;
         private readonly IMapper _mapper;
         private readonly UserManager<AppUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        public CreateUserHandler(IRepositoryAsync<AppUser> repositoryAsync, IMapper mapper, RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        public CreateUserHandler(IRepositoryAsync<AppUser> repositoryAsync, IRepositoryAsync<Domain.Entities.User> repositoryAsync1,  IMapper mapper, RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
         {
             _repositoryAsync = repositoryAsync;
+            _repositoryAsync1 = repositoryAsync1;
             _mapper = mapper;
             _roleManager = roleManager;
             _userManager = userManager;
@@ -57,14 +61,27 @@ namespace Application.Features.User.Handlers
 
             if (!resultSet.Succeeded)
                 throw new ValidationException(resultSet.Errors.ToString());
-            
+
+            var userClaims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            };
+            await _userManager.AddClaimsAsync(user, userClaims);
             if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult())
             {
                 await _roleManager.CreateAsync(new IdentityRole("admin"));
                 await _roleManager.CreateAsync(new IdentityRole("basic"));
             }
 
+            Domain.Entities.User us = new()
+            {
+                IdAccount = user.Id,
+            };
+            var value = await _repositoryAsync1.AddAsync(us, cancellationToken);
             await _userManager.AddToRoleAsync(user, request.Role);
+                
 
             var userReturn = _mapper.Map<UserDatosDto>(user);
             return new Response<UserDatosDto>(userReturn);
