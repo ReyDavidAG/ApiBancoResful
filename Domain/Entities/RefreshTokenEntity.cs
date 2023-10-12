5b79e741a7218cc2bfdc567e3930bb018ff0ab43
ReyDavidAG <david.aguila@proan.com>
2023-10-12 16:07:26 -0600

    Add Entity - RefreshToken

---
+++ Domain/Entities/RefreshTokenEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities;
+
+public class RefreshTokenEntity
+{
+    public Guid IdRefresh { get; set; }
+    public string AppUserId { get; set; }
+    public string? Token { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Expires { get; set; }
+    public bool IsExpired => DateTime.Now > Expires;
+    public string ReplacedToken { get; set; }
+}
